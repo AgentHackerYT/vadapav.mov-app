52e09ce09e0f5e094e56241c8b61f5ff5d6044ea
Jenil <43415449+AgentHackerYT@users.noreply.github.com>
2024-02-09 16:59:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const electron = require('electron')
+const path = require('path')
+
+const app = electron.app
+function createWindow () {
+  const win = new electron.BrowserWindow({
+    autoHideMenuBar: true,
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload:path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('public/watch.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
